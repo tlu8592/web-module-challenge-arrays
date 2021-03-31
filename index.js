78380a94d70214fc6d5d2619ae89283485fd8aa5
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-03-31 00:41:39 -0400

    Test passed: filterByWord

--- index.js
+++ index.js
@@ -172,10 +172,17 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr, string){
+    let results = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes(string)) {
+            results.push(arr[i]);
+        }
+    }
+    return results;
 }
 
+console.log(filterByWord(originalFlavors, "Chocolate"));
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
